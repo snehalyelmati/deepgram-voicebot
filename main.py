2d8fb7f2818529cef499f6e86ca898fabbf7a68a
Snehal Reddy Yelmati <snehalyelmati@gmail.com>
2023-04-26 12:51:27 +0530

    Transform Deepgram output with speaker diarization

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM, BloomTok
 # Creating a Flask app
 app = Flask(__name__)
 
+
 # os.environ["PYTORCH_MPS_HIGH_WATERMARK_RATIO"]="0.0"
 
 
@@ -33,9 +34,9 @@ def speech_to_text():
         try:
             # Transcribe to text using Deepgram
             # TODO: try out speaker diarization
-            verbatim, summaries = asyncio.run(deepgram_stt(save_path))
-
-            # pass it to NLU
+            response = asyncio.run(deepgram_stt(save_path))
+            verbatim, summaries = response["results"]["channels"][0]["alternatives"][0]["transcript"], \
+                response["results"]["channels"][0]["alternatives"][0]["summaries"]
 
             return jsonify(
                 {
@@ -80,11 +81,61 @@ async def deepgram_stt(FILE: str):
         )
     )
 
-    print(
-        f'Speech to text based on audio input: {json.dumps(response, indent=4)}')
+    print(f'Speech to text based on audio input: {json.dumps(response, indent=4)}')
     # return response["results"]["channels"][0]["alternatives"][0]["transcript"]
-    return response["results"]["channels"][0]["alternatives"][0]["transcript"], \
-        response["results"]["channels"][0]["alternatives"][0]["summaries"]
+    # return response["results"]["channels"][0]["alternatives"][0]["transcript"], \
+    #     response["results"]["channels"][0]["alternatives"][0]["summaries"]
+    return response
+
+
+@app.route('/fraud_detection', methods=['POST'])
+def fraud_detection():
+    if request.method == 'POST':
+        conv_id = request.form["conversation_id"]
+        dirname = "./audio_files/"
+        save_path = os.path.join(dirname, conv_id + ".mp3")
+        request.files['audio_file'].save(save_path)
+
+        try:
+            # Transcribe to text using Deepgram
+            response = asyncio.run(deepgram_stt(save_path))
+            print(f'Speech to text based on audio input: {json.dumps(response, indent=4)}')
+
+            transcript = []
+            idx = 0
+
+            temp = {
+                'speaker': 0,
+                'utterance': "",
+            }
+            transcript.append(temp)
+
+            words = response["results"]["channels"][0]["alternatives"][0]["words"]
+            for i in range(len(words)):
+                if i == 0:
+                    continue
+
+                if words[i - 1]["speaker"] == words[i]["speaker"]:
+                    print(transcript)
+                    transcript[idx]["utterance"] += words[i]["punctuated_word"] + " "
+                else:
+                    idx += 1
+                    temp = {
+                        'speaker': words[i]["speaker"],
+                        'utterance': words[i]["punctuated_word"] + " ",
+                    }
+                    transcript.append(temp)
+
+            return jsonify({
+                # 'response': response,
+                'transcript': transcript,
+            })
+        except Exception as e:
+            exception_type, exception_object, exception_traceback = sys.exc_info()
+            line_number = exception_traceback.tb_lineno
+            print(f'line {line_number}: {exception_type} - {e}')
+
+        return jsonify({'result': 'Something wrong with Deepgram API'})
 
 
 @app.route('/llm_initialize', methods=['GET'])
@@ -119,13 +170,12 @@ def llm_save():
         tokenizer = BloomTokenizerFast.from_pretrained(model_name)
         model = BloomForCausalLM.from_pretrained(model_name)
 
-        print(f'Save path: {save_path+model_name}')
-        model.save_pretrained(save_path+model_name)
-        tokenizer.save_pretrained(save_path+model_name)
+        print(f'Save path: {save_path + model_name}')
+        model.save_pretrained(save_path + model_name)
+        tokenizer.save_pretrained(save_path + model_name)
         print(f'Saving model finished...')
 
         return jsonify({'result': 'success'})
-
 
 
 # TODO: add proper loggers
