fc5cff8694281707a2417165ea0cd3bc9330fc37
Snehal Reddy Yelmati <snehalyelmati@gmail.com>
2023-04-26 09:26:29 +0530

    Implement LLM inference endpoint

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ import sys
 
 from deepgram import Deepgram
 from flask import Flask, jsonify, request
+from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM, BloomTokenizerFast, BloomForCausalLM
 
 # Creating a Flask app
 app = Flask(__name__)
+
+os.environ["PYTORCH_MPS_HIGH_WATERMARK_RATIO"]="0.0"
 
 
 # To check the if the API is up
@@ -82,6 +85,58 @@ async def deepgram_stt(FILE: str):
     # return response["results"]["channels"][0]["alternatives"][0]["transcript"]
     return response["results"]["channels"][0]["alternatives"][0]["transcript"], \
         response["results"]["channels"][0]["alternatives"][0]["summaries"]
+
+
+@app.route('/llm_initialize', methods=['GET'])
+def llm_intialize():
+    if request.method == 'GET':
+        global tokenizer
+        global model
+        global device
+
+        # path = "bigscience/bloomz-560m"
+        # path = "bigscience/bloomz-1b7"
+        # path = "bigscience/bloomz-3b"
+        path = "bigscience/bloomz-7b1"
+
+        device = "mps"
+
+        print(f'Loading model started from path: {path}')
+        tokenizer = BloomTokenizerFast.from_pretrained(path)
+        model = BloomForCausalLM.from_pretrained(path)
+        model.to(device)
+        print(f'Loading model finished...')
+
+        return jsonify({'result': 'success'})
+
+
+# TODO: add proper loggers
+def llm(input: str):
+    # generator = pipeline("text2text-generation", model, tokenizer, device=device)
+    # print(f'Pipeline output: {generator(input, max_length=150, num_return_sequences=1)}')
+
+    # inputs = tokenizer('''
+    #     Advice from a financial adviser: "Invest in company XYZ, the returns would triple in an year."
+
+    #     Based on this advice, answer the below question,
+    #     Question: Is the advice fradulent? If yes, return "yes", else return "no"
+
+    #     Answer: ''', return_tensors="pt")["input_ids"].to(device)
+
+    # inputs = tokenizer('A cat in French is "', return_tensors="pt")["input_ids"].to(device)
+    inputs = tokenizer(input, return_tensors="pt")["input_ids"].to(device)
+    outputs = model.generate(inputs, max_new_tokens=3)
+    decoded_output = tokenizer.decode(outputs[0])
+    print(f'Decoded output: {decoded_output}')
+    return decoded_output
+
+
+@app.route('/llm_predict', methods=['POST'])
+def llm_predict():
+    if request.method == 'POST':
+        data = request.form["utterance"]
+        result = llm(str(data))
+        return jsonify({'result': result})
 
 
 # driver function
